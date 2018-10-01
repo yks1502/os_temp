7b878982fffe9024f9c4e69799699e11b7264642
cobaltblu27 <cobaltblu27@gmail.com>
2018-10-01 20:53:58 +0900

    tried increasing NR_syscalls in unistd.h

--- linux-3.10-artik/include/linux/syscalls.h
+++ linux-3.10-artik/include/linux/syscalls.h
@@ -64,7 +64,7 @@ struct old_linux_dirent;
 struct perf_event_attr;
 struct file_handle;
 struct sigaltstack;
-
+struct prinfo;
 #include <linux/types.h>
 #include <linux/aio_abi.h>
 #include <linux/capability.h>
@@ -846,5 +846,6 @@ asmlinkage long sys_process_vm_writev(pid_t pid,
 asmlinkage long sys_kcmp(pid_t pid1, pid_t pid2, int type,
 			 unsigned long idx1, unsigned long idx2);
 asmlinkage long sys_finit_module(int fd, const char __user *uargs, int flags);
+
 asmlinkage int ptree(struct prinfo *buf, int *nr);
 #endif
